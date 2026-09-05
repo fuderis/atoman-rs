1bd65f9a6797b383926b2667c36f2c520ce18d7a
Bulat Sh. (fuderis) <synapdrake@ya.ru>
2026-09-05 05:21:58 +0500

    update config macros

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -8,21 +8,21 @@ pub fn config(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStruct);
     let struct_name = &input.ident;
 
-    // generate the name of the static variable: Settings -> SETTINGS_CONFIG (or SETTINGS)
+    // generate name of the static variable: Settings -> SETTINGS_CONFIG (or SETTINGS)
     let static_name_str = format!("{}_CONFIG", struct_name.to_string().to_shouty_snake_case());
     let static_ident = syn::Ident::new(&static_name_str, struct_name.span());
 
     let expanded = quote! {
-        // preserve the original structure (with all its attributes and fields).
+        // preserve original structure
         #input
 
-        // generating a static status store
+        // static status store
         static #static_ident: atoman::State<atoman::Config<#struct_name>> = atoman::State::default();
 
-        // generate a helper impl for the structure.
+        // helper impl for the structure.
         impl #struct_name {
             /// Reads & initializes the config
-            pub async fn init<P>(file_path: P) -> ::std::result::Result<(), Box<dyn std::error::Error + Send + Sync +'static>>
+            pub async fn init<P>(file_path: P) -> ::std::result::Result<(), Box<dyn std::error::Error + Send + Sync>>
             where
                 P: ::std::convert::AsRef<::std::path::Path>,
             {
@@ -47,18 +47,18 @@ pub fn config(_attr: TokenStream, item: TokenStream) -> TokenStream {
             }
 
             /// Returns config state guard
-            pub async fn lock() -> atoman::StateGuard<atoman::Config<#struct_name>> {
+            pub async fn lock() -> atoman::StateGuard<'static, atoman::Config<#struct_name>> {
                 #static_ident.lock().await
             }
 
             /// Returns actual config file data
-            pub async fn read() -> ::std::result::Result<atoman::Config<#struct_name>, Box<dyn std::error::Error + Send + Sync +'static>> {
+            pub async fn read() -> ::std::result::Result<atoman::Config<#struct_name>, Box<dyn std::error::Error + Send + Sync>> {
                 let path = #static_ident.dirty_get().path().clone();
                 atoman::Config::<#struct_name>::read(path).await
             }
 
             /// Updates config from file
-            pub async fn update() -> ::std::result::Result<bool, Box<dyn std::error::Error + Send + Sync +'static>> {
+            pub async fn update() -> ::std::result::Result<bool, Box<dyn std::error::Error + Send + Sync>> {
                 let mut cfg = #static_ident.lock().await;
 
                 if cfg.check(0).await? {
